3f256ab2656d789f24525c6d6f724915e41c8890
bryan <bryan.zack@zacklabs.com>
2024-07-12 15:32:25 -0400

    added todo list for shuffling logic

--- cogs/jumble.py
+++ cogs/jumble.py
@@ -26,6 +26,7 @@ class Jumble(commands.Cog, name="jumble"):
         name="jumble",
         description="Album name guessing game",
     )
+    # when someone types the .jumble command, do the following, command prefix is changed in config.json file
     async def px(self, context: Context) -> None:
         id = context.author.id
         # Attempt to find local sqlite3 database
@@ -44,10 +45,21 @@ class Jumble(commands.Cog, name="jumble"):
                 #user has account linked, we will now run the last.fm api logic
                 else:
                     await context.send('user has linked last.fm account,')
-                    # getTopAlbums
                     albums = requests.get(f"{os.getenv('API_ROOT')}/?method=user.gettopalbums&user={select_rows[0][1]}&api_key={os.getenv('LASTFM_KEY')}&format=json")
-                    print(albums.text)
-                    await context.send(albums.text)
+                    # TODO:
+                    # 1.) turn response into navigable json
+                    # 2.) get length of albums array
+                    # 3.) get random number between 0 and that same length
+                    # 4.) use json indexing to grab the randomly picked album
+                    # 5.) get the albums title as a string
+                    # 6.) splice the title into an array of words, with a blank space as the separator
+                    # 7.) shuffle the letters in each word
+                    # 8.) reattach the shuffled words into a string with spaces in their original spots
+                    # 9.) print the shuffled word back to the server
+
+
+                    #print(albums.text)
+                    #await context.send(albums.text)
 
 
 
