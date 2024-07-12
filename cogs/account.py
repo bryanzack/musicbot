f6f32b6d41be83c58c65cfb12f3cfa90592c635b
bryan <bryan.zack@zacklabs.com>
2024-07-12 15:05:47 -0400

    added comments + last.fm account linkage to local sql database

--- cogs/account.py
+++ cogs/account.py
@@ -24,22 +24,31 @@ class Account(commands.Cog, name="account"):
         description="links last.fm account name to bot database.",
     )
     async def link(self, context: Context, arg) -> None:
+        #discord ID of the command author
         id = context.author.id
         print(f"id: {id}")
         print(f"fmname: {arg}")
+        # attempt to connect to sqlite3 database
         async with aiosqlite.connect(
             f"{os.path.realpath(os.path.dirname(__file__))}/../database/database.db"
         ) as db:
+            # check to see if command author's discord id exists in database
             async with db.execute(f"SELECT * FROM User WHERE Discord_ID = {id}") as select_cursor:
                 select_rows = await select_cursor.fetchall()
                 print(select_rows)
+                # if select_rows is zero, no records were found & user does not exist
                 if (len(select_rows) == 0):
+                    # insert user into database and commit changes
                     await db.execute(f"INSERT INTO User values ({id}, '{arg}')")
                     await db.commit()
                     await context.send(f'user linked last.fm account "{arg}"')
+                # if select rows is not zero, it must be one, meaning user exists in the database already
+                # if user exists in database already and is trying to link, they must want to link a different account
                 else:
-                    print('name test')
                     print(arg)
+                    # select_rows will be an array of (123413421342134, "marceline80")
+                    # where the first number is the users discord id, and the second is the users new last.fm name
+                    # that they want to relink to
                     print(select_rows[0][1])
                     if (arg != select_rows[0][1]):
                         await db.execute(f"UPDATE User SET Lastfm_Name = '{arg}' WHERE Discord_ID = {id}")
